0181417a8143435c2459590d0184751156f21fdc
criticalzero123 <jdcaramonte159@gmail.com>
2023-11-22 02:18:47 +0800

    Add realtor model

--- backend/model/realtorModel.js
+++ backend/model/realtorModel.js
@@ -1,16 +1,64 @@
 const createPromisePool = require("../helper/mysqlPromise");
+const bcrypt = require("bcrypt");
 
 class RealtorModel {
   constructor() {
     this.pool = createPromisePool();
   }
 
-  async getRealtors() {
-    const [rows] = await this.pool.query("SELECT * FROM realtor");
-    return rows;
+  //   GET USER
+  async getRealtorByEmail(email) {
+    const [rows] = await this.pool.query(
+      "SELECT * FROM realtor WHERE email = ?",
+      [email]
+    );
+    return rows[0];
   }
 
-  // You can add more methods for creating, updating, and deleting users
+  async getRealtorById(realtorId) {
+    const [rows] = await this.pool.query("SELECT * FROM realtor WHERE id = ?", [
+      realtorId,
+    ]);
+    return rows[0];
+  }
+
+  // LOGIN
+  async authenticate(email, password) {
+    const realtor = await this.getRealtorByEmail(email);
+
+    if (!realtor) {
+      return null;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, realtor.pass);
+
+    return isPasswordValid ? realtor : null;
+  }
+
+  // CDU
+  async createRealtor(realtor) {
+    const hashedPassword = await bcrypt.hash(realtor.password, 10);
+
+    await this.pool.query(
+      "INSERT INTO users (fname, mname,lname,email,pass,birthdate) VALUES (?, ?, ? ,? ,? ,?)",
+      [
+        realtor.firstName,
+        realtor.middleName,
+        realtor.lastName,
+        realtor.email,
+        hashedPassword,
+        realtor.birthdate,
+      ]
+    );
+  }
+
+  //   TODO:No Update Yet
+
+  //TODO: Not yet finish
+  //Note: Dont use ID this is not secure. Change this.
+  async deleteRealtorById(realtorId) {
+    await this.pool.query("DELETE FROM realtor WHERE id = ?", [realtorId]);
+  }
 }
 
 module.exports = RealtorModel;
