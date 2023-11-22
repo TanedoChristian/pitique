6d3d1607cf3506aa65f0b245df8006242b0b8627
criticalzero123 <jdcaramonte159@gmail.com>
2023-11-23 00:39:38 +0800

    Add portfolio model

---
+++ backend/model/portfolioModel.js
@@ -0,0 +1,38 @@
+const createPromisePool = require("../helper/mysqlPromise");
+
+class PortfolioModel {
+  constructor() {
+    this.pool = createPromisePool();
+  }
+
+  //   GET Portfolio
+
+  async getPackageById(portfolioId) {
+    const [rows] = await this.pool.query(
+      "SELECT * FROM portfolio WHERE id = ?",
+      [portfolioId]
+    );
+    return rows[0];
+  }
+
+  // CDU
+  async createPortfolio(portfolio) {
+    // Default Value
+    const visibility = true;
+
+    await this.pool.query(
+      "INSERT INTO portfolio (ptqr_id,img,isvisible) VALUES (?, ?, ?)",
+      [portfolio.pitiquerId, portfolio.img, visibility]
+    );
+  }
+
+  //Remove
+  async removePortfolioById(portfolioId, pitiquerId, visibility) {
+    await this.pool.query(
+      "UPDATE FROM portfolio SET isvisible = ? WHERE id = ? and ptqr_id = ?",
+      [visibility, portfolioId, pitiquerId]
+    );
+  }
+}
+
+module.exports = PortfolioModel;
