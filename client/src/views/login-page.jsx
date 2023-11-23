01cf63eab410c8b08a610239bad6ee8b8d3fbae5
criticalzero123 <jdcaramonte159@gmail.com>
2023-11-23 17:02:18 +0800

    Add login

--- client/src/views/login-page.jsx
+++ client/src/views/login-page.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import api from "../helper/api";
+
 const LoginPage = () => {
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await api.post("/realtors/login", user);
+
+    console.log(data);
+  };
+
   return (
     <div className="w-full h-screen flex justify-between flex-col poppins tracking-wide">
       <div className="h-[40vh] w-full  bg-cyan-400 rounded-b-[50%] flex items-center  justify-center ">
@@ -8,18 +29,22 @@ const LoginPage = () => {
       </div>
 
       <div className="p-10">
-        <form className="flex flex-col gap-3">
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
           <h1 className="text-lg font-bold">Login </h1>
           <input
             type="text"
             placeholder="Email"
             className="p-3 bg-gray-200 w-full rounded-sm"
+            onChange={handleChange}
+            name="email"
           />
 
           <input
             type="password"
             placeholder="Password"
             className="p-3 bg-gray-200 w-full rounded-sm "
+            name="password"
+            onChange={handleChange}
           />
 
           <button className="mt-5 p-3 w-full bg-cyan-400 text-white font-bold rounded-sm shadow-md">
