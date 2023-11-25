e80c90944223eae03eb00d9508bf07f81e247ed1
criticalzero123 <jdcaramonte159@gmail.com>
2023-11-25 18:22:31 +0800

    Add booking details in pitiquer books

--- client/src/components/pitique-booking-layout/layout.jsx
+++ client/src/components/pitique-booking-layout/layout.jsx
@@ -2,8 +2,27 @@ import Header from "../common/header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import PitiqueBookingCard from "./pitique-booking-card";
+import { useEffect, useState } from "react";
+import api from "../../helper/api";
 
 const PitiqueBookingLayout = () => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      // CHANGE The 1 to current user
+      try {
+        const { data } = await api.get("/bookings/pitiquer/1");
+
+        setBookings(data);
+      } catch (error) {
+        alert("No bookings found");
+        console.error(error);
+      }
+    };
+
+    fetch();
+  }, []);
   return (
     <div>
       <Header className="flex items-center p-5 gap-5">
@@ -19,7 +38,8 @@ const PitiqueBookingLayout = () => {
       </Header>
 
       <div className="w-full p-3">
-        <PitiqueBookingCard />
+        {bookings.length !== 0 &&
+          bookings.map((booking) => <PitiqueBookingCard booking={booking} />)}
       </div>
     </div>
   );
