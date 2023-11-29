d9a9e73e7187d94dd0c7b79b815da25230503bd8
criticalzero123 <jdcaramonte159@gmail.com>
2023-11-29 23:18:08 +0800

    Fix display in transactio realtor

--- client/src/components/realtor-transaction-history-layout/realtor-transaction-incoming.jsx
+++ client/src/components/realtor-transaction-history-layout/realtor-transaction-incoming.jsx
@@ -18,38 +18,40 @@ const RealtorTransactionIncoming = ({ data, refresh }) => {
 
   return (
     data &&
-    data.length !== 0 &&
-    data.map((booking) => (
-      <div className="" key={booking.id}>
+    data.length !== 0 && (
+      <div>
         <h1 className="text-xl text-cyan-500 font-bold">Booking In-progress</h1>
-
-        <div className="w-full bg-gray-200 p-3 flex justify-between items-center mt-3 rounded-md">
-          <Link
-            to={`/booking/${booking.id}`}
-            className="flex gap-3 items-center"
-          >
-            <img
-              className="w-9 h-9  rounded-full "
-              src="https://cdn-icons-png.flaticon.com/512/5605/5605056.png"
-            />
-            <div>
-              <h1 className="font-bold text-sm">Php {booking.total}</h1>
-              <p className="text-sm">
-                {booking.fname} {booking.lname}
-              </p>
-              <p className="text-sm">
-                {booking.pkg_desc} -{" "}
-                {new Date(booking.date).toLocaleString("en-us", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}
-              </p>
+        {data.map((booking) => (
+          <div className="" key={booking.id}>
+            <div className="w-full bg-gray-200 p-3 flex justify-between items-center mt-3 rounded-md">
+              <Link
+                to={`/booking/${booking.id}`}
+                className="flex gap-3 items-center"
+              >
+                <img
+                  className="w-9 h-9  rounded-full "
+                  src="https://cdn-icons-png.flaticon.com/512/5605/5605056.png"
+                />
+                <div>
+                  <h1 className="font-bold text-sm">Php {booking.total}</h1>
+                  <p className="text-sm">
+                    {booking.fname} {booking.lname}
+                  </p>
+                  <p className="text-sm">
+                    {booking.pkg_desc} -{" "}
+                    {new Date(booking.date).toLocaleString("en-us", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </p>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
+          </div>
+        ))}
       </div>
-    ))
+    )
   );
 };
 
