32ddaeed5f9c9b264500eabd219bfa8090ba8cf7
criticalzero123 <jdcaramonte159@gmail.com>
2023-11-29 23:16:19 +0800

    Fetch and navigate to booking in realtor notif

--- client/src/helper/notification-message.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const notif = async (bookId, message) => {
-  try {
-    await api.post("/notifications", { book_id: bookId, message });
-  } catch (error) {
-    console.error(error);
-  }
-};
